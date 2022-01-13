05b39f013c08d9551f0f8b98f1ca5c92a850f811
abdoul6 <abdoul6@schule.bremen.de>
2022-01-13 15:20:42 +0100

    KlassenApfel & bananen

---
+++ Obst/src/Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    public static void main(String[] args) {
+        Apfel apfel = new Apfel();
+        apfel.setFarbe("grün");
+        apfel.setGewicht(250);
+        apfel.setReif(true);
+
+        System.out.println(apfel.getName());
+        System.out.println(apfel.getGewicht());
+
+    }
+}
